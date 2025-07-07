3fb491f4cf5ceaaddcd41ec896e0a7f6e459cf7f
Salvador HM <salvadorhm@gmail.com>
2025-07-07 19:19:30 +0000

    FIXED 08_send_files

--- demos/08_sendfile/bot.py
+++ demos/08_sendfile/bot.py
@@ -1,8 +1,31 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import ForceReply, Update
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes, filters
+from dotenv import load_dotenv
 import os
 
-async def send_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
+load_dotenv()
+token=os.getenv("TELEGRAM_API_KEY")
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Este bot puede realizar las siguientes acciones:")
+
+
+async def echo_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+async def send_file_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     Envía un archivo al usuario que llama al comando /sendfile.
     """
@@ -23,11 +46,16 @@ async def send_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Ocurrió un error al intentar enviar el archivo.")
 
 if __name__ == '__main__':
-    # Reemplaza 'TU_TOKEN' con el token real de tu bot
-    app = ApplicationBuilder().token("TU_TOKEN").build()
+    # Reemplaza 'TELEGRAM_API_KEY' con el token real de tu bot en el archivo .env
+    app = ApplicationBuilder().token(token).build()
+
+    # on different commands - answer in Telegram
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo_command))
 
     # Manejador para el comando /sendfile
-    app.add_handler(CommandHandler("sendfile", send_file))
+    app.add_handler(CommandHandler("sendfile", send_file_command))
 
     # Inicia el bot en modo polling
     print("Bot iniciado. Usa el comando /sendfile para recibir un archivo.")
